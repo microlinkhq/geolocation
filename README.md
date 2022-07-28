df4023b211d8d04a8370790f6bc47ecb9ec7e01c
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-07-28 11:01:22 +0200

    build: first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<div align="center">
+  <img src="https://cdn.microlink.io/logo/banner.png">
+  <br>
+  <br>
+</div>
+
+> Get detailed information about the incoming request based on the IP address.
+
+## License
+
+**microlink** © [Microlink](https://microlink.io), released under the [MIT](https://github.com/microlinkhq/sdk/blob/master/LICENSE.md) License.<br>
+Authored and maintained by [Kiko Beats](https://kikobeats.com) with help from [contributors](https://github.com/microlinkhq/sdk/contributors).
+
+> [microlink.io](https://microlink.io) · GitHub [microlinkhq](https://github.com/microlinkhq) · Twitter [@microlinkhq](https://twitter.com/microlinkhq)
---
+++ scripts/postinstall.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const { eeaMember, euMember } = require('is-european')
+const { writeFile, mkdir } = require('fs/promises')
+const iso31661 = require('iso-3166')
+const path = require('path')
+
+const got = require('got').extend({
+  responseType: 'json',
+  resolveBodyOnly: true
+})
+
+const URL =
+  'https://raw.githubusercontent.com/mledoze/countries/master/dist/countries.json'
+
+const DESTINATION_FOLDER = path.resolve(__dirname, '../src')
+const DESTINATION_PATH = path.resolve(DESTINATION_FOLDER, 'countries.json')
+
+const toData = payload =>
+  payload.map(item => {
+    const {
+      flag,
+      cca2: alpha2,
+      callingCodes,
+      languages,
+      currencies,
+      tld
+    } = item
+
+    const { alpha3, numeric } =
+      iso31661.find(item => item.alpha2 === 'ES') || {}
+
+    return {
+      alpha2,
+      alpha3,
+      callingCodes,
+      currencies,
+      eeaMember: eeaMember(alpha2),
+      euMember: euMember(alpha2),
+      flag,
+      languages,
+      numeric: Number(numeric),
+      tld
+    }
+  })
+
+got(URL)
+  .then(async data => {
+    await mkdir(DESTINATION_FOLDER).catch(() => {})
+    await writeFile(DESTINATION_PATH, JSON.stringify(toData(data), null, 2))
+    console.log(`Added ${data.length} countries ✨`)
+  })
+  .then(process.exit)
+  .catch(err => console.error(err) && process.exit(1))
