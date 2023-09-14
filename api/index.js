3b15341881caea8e16ad9a7a33ad8610dd6f270a
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-09-14 13:16:42 +0200

    fix: improve city accuracy

--- api/index.js
+++ api/index.js
@@ -62,8 +62,8 @@ export default async req => {
   const payload = {
     ip: toIP(address),
     city: {
-      name: getCity(headers.get('x-vercel-ip-city')),
-      alpha2: `${countryAlpha2}-${headers.get('x-vercel-ip-country-region')}`
+      name: getCity(headers.get('cf-ipcity') ?? headers.get('x-vercel-ip-city'))
+      // alpha2: `${countryAlpha2}-${headers.get('x-vercel-ip-country-region')}`
     },
     country,
     continent,
@@ -75,10 +75,10 @@ export default async req => {
     languages,
     tlds,
     coordinates: {
-      latitude: Number(headers.get('x-vercel-ip-latitude')),
-      longitude: Number(headers.get('x-vercel-ip-longitude'))
+      latitude: headers.get('cf-iplatitude') ?? headers.get('x-vercel-ip-latitude'),
+      longitude: headers.get('cf-iplongitude') ?? headers.get('x-vercel-ip-longitude')
     },
-    timezone: headers.get('x-vercel-ip-timezone')
+    timezone: headers.get('cf-timezone') ?? headers.get('x-vercel-ip-timezone')
   }
 
   if (searchParams.get('headers') !== null) {
