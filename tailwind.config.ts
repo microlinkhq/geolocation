fd10aad83290fd18f793ab087da0b6e1336263b5
Jose Francisco 'Kiko' Verdú Gambín <josefrancisco.verdu@gmail.com>
2026-04-28 10:54:46 +0300

    chore: tailwind v4 (#41)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import tailwindcssAnimate from 'tailwindcss-animate'
 
 const config: Config = {
   darkMode: ['class'],
@@ -91,6 +92,6 @@ const config: Config = {
       }
     }
   },
-  plugins: [require('tailwindcss-animate')]
+  plugins: [tailwindcssAnimate]
 }
 export default config
